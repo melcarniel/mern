ced81d938d95e6f88e6c278fd4cb3727e9f48527
Melina Carniel <Melina.Carniel@targetsistemas.com.br>
2019-05-15 16:09:11 -0300

    profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,4 +131,89 @@ router.post('/', [auth, [
  
 });
 
+//@route  DELETE api/profile
+//@desc   Deletar profile, user e posts
+//@access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo remover posts
+    //remove profile
+    await Profile.findOneAndRemove({user: req.user.id});
+    //remove user
+    await User.findOneAndRemove({_id: req.user.id});
+
+    res.json({msg: 'Usuário removido!'})
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Erro de servidor!');
+  }
+});
+
+//@route  PUT api/profile/experience
+//@desc   Atualizar profile experience, education
+//@access Private
+router.put('/experience', [auth, [
+  check('title', 'Título é obrigatório').not().isEmpty(),
+  check('company', 'Empresa é obrigatório').not().isEmpty(),
+  check('from', 'Início é obrigatório').not().isEmpty()
+
+]], async (req, res) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { title, company, location, from, to, current, description } = req.body;
+
+  const newExperience = {
+    //title: title - mesma coisa que colocar só a variável
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //unshift tem a mesma função do push
+    profile.experience.unshift(newExperience);
+
+    await profile.save();
+
+    res.json(profile);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Erro de servidor!');
+  }
+});
+
+//@route  DELETE api/profile/experience/:exp_id
+//@desc   Deletar uma experience
+//@access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //pegar id da experience
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Erro de servidor!')
+  }
+});
+
+
 module.exports = router;

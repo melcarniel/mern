53a5c2ececc3f7a9984abef7bbc2ed6ca110036c
Melina Carniel <Melina.Carniel@targetsistemas.com.br>
2019-05-21 09:52:54 -0300

    Front redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
+  <Provider store={store}>
+    <Router>
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing}></Route>
       <section className="container">
+        <Alert />
         <Switch>
           <Route exact path='/register' component={Register}></Route>
           <Route exact path='/login' component={Login}></Route>
@@ -19,6 +27,8 @@ const App = () => (
       </section>
     </Fragment>
   </Router>
+  </Provider>
+  
   
 );
     
